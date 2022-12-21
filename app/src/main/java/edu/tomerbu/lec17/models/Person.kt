8b9d5aab8b29452a5b3585ecb959b36e43e80dc4
BuTomer <bu.tomer@gmail.com>
2022-12-21 19:22:00 +0200

    Added Room Database

---
+++ app/src/main/java/edu/tomerbu/lec17/models/Person.kt
@@ -0,0 +1,11 @@
+package edu.tomerbu.lec17.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "people")
+data class Person(
+    val name: String,
+    @PrimaryKey val personId: String = UUID.randomUUID().toString(),
+)
