a4ec5cbe4ae82a7606329710b7e59f0b1e466c04
BuTomer <bu.tomer@gmail.com>
2023-01-04 22:14:30 +0200

    remove text ref

--- app/src/main/java/edu/tomerbu/lec17/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/edu/tomerbu/lec17/ui/gallery/GalleryFragment.kt
@@ -30,9 +30,7 @@ class GalleryFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
